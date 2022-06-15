19d450d939635d756add739e78118fc3fdada282
falook555 <falook555@gmail.com>
2022-06-15 14:12:03 +0700

    ssaaaa

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
-module.exports = {
-    secret: '1234',
-    dbOption: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'todolist',
-        port: '3306'
-    }
-}
 // module.exports = {
 //     secret: '1234',
 //     dbOption: {
 //         host: 'localhost',
-//         user: 'sukkla01',
-//         password: '0810432245',
-//         database: 'running2019',
+//         user: 'root',
+//         password: '',
+//         database: 'todolist',
 //         port: '3306'
 //     }
 // }
+module.exports = {
+    secret: '1234',
+    dbOption: {
+        host: '192.168.0.32',
+        user: 'ict009',
+        password: 'Ss0810432245',
+        database: 'todolist',
+        port: '3306'
+    }
+}
